1b7c632d756e5aaadbb9bb5c68978d39573d8c51
emonmahdi <developeremon1997@gmail.com>
2024-03-03 11:23:52 +0600

    task content and my task UI done

---
+++ src/components/tasks/MyTasks.jsx
@@ -0,0 +1,15 @@
+const MyTasks = () => {
+  return (
+    <div className="flex justify-between items-center bg-gray-300 rounded-lg p-4 mt-4 mb-2">
+      <div>
+        <p> Add the User</p>
+      </div>
+      <div>
+        <span>icon</span>
+        <span>icon</span>
+      </div>
+    </div>
+  );
+};
+
+export default MyTasks;
