2a38051d3b009066ae79e8e5943ca7b830f44d13
emonmahdi <developeremon1997@gmail.com>
2024-03-03 12:36:23 +0600

    complete UI design

--- src/components/tasks/MyTasks.jsx
+++ src/components/tasks/MyTasks.jsx
@@ -1,12 +1,38 @@
+import {
+  CheckIcon,
+  DocumentMagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
+
 const MyTasks = () => {
+  const item = {
+    id: 1,
+    status: "pending",
+    title: "Remove Button",
+    description:
+      "We need a remove button in our task card. Meke the button red and use Heroicon for tashbin icon.",
+    date: "2023-08-28",
+    assignedTo: "Mir Hussain",
+    priority: "high",
+  };
+
   return (
-    <div className="flex justify-between items-center bg-gray-300 rounded-lg p-4 mt-4 mb-2">
-      <div>
-        <p> Add the User</p>
-      </div>
-      <div>
-        <span>icon</span>
-        <span>icon</span>
+    <div>
+      <h1 className="text-xl my-3">My Tasks</h1>
+      <div className=" h-[750px] overflow-auto space-y-3">
+        <div
+          key={item.id}
+          className="bg-secondary/10 rounded-md p-3 flex justify-between"
+        >
+          <h1>{item.title}</h1>
+          <div className="flex gap-3">
+            <button className="grid place-content-center" title="Details">
+              <DocumentMagnifyingGlassIcon className="w-5 h-5 text-primary" />
+            </button>
+            <button className="grid place-content-center" title="Done">
+              <CheckIcon className="w-5 h-5 text-primary" />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
