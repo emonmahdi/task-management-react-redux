0ba7cfb6f9baeeca59f55f144b20f5486c2da6a8
emonmahdi <developeremon1997@gmail.com>
2024-03-07 11:55:55 +0600

    create user slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import taskReducer from "./features/task/taskSlice";
+import userSlice from "./features/user/userSlice";
 
 const store = configureStore({
   reducer: {
     taskSlice: taskReducer,
+    userSlice: userSlice,
   },
 });
 
