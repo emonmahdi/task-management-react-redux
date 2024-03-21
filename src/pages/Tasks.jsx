e7d2bedc96351f6b29cb74176d7d44357b447e1f
emonmahdi <developeremon1997@gmail.com>
2024-03-21 15:06:52 +0600

    getTask using rtk query

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -3,17 +3,17 @@ import MyTasks from "../components/tasks/MyTasks";
 import TaskCard from "../components/tasks/TaskCard";
 import { useState } from "react";
 import AddTaskModal from "../components/tasks/AddTaskModal";
-import { useSelector } from "react-redux";
 import MenuDropdown from "../components/ui/MenuDropdown";
+import { useGetTasksQuery } from "../redux/features/api/baseApi";
 
 const Tasks = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const { tasks } = useSelector((state) => state.taskSlice);
+  const { data: tasks } = useGetTasksQuery();
 
-  const pendingTasks = tasks.filter((item) => item.status === "pending");
-  const runningTasks = tasks.filter((item) => item.status === "running");
-  const doneTasks = tasks.filter((item) => item.status === "done");
+  const pendingTasks = tasks?.filter((item) => item.status === "pending");
+  const runningTasks = tasks?.filter((item) => item.status === "running");
+  const doneTasks = tasks?.filter((item) => item.status === "done");
 
   return (
     <div className="h-screen grid grid-cols-12">
@@ -56,7 +56,7 @@ const Tasks = () => {
             <div className="flex sticky top-0 justify-between bg-purple-500 text-white p-5 rounded-md mb-3">
               <h1>Up Next</h1>
               <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
-                {pendingTasks.length}
+                {pendingTasks?.length}
               </p>
             </div>
             <div className="space-y-3">
@@ -69,7 +69,7 @@ const Tasks = () => {
             <div className="flex sticky top-0 justify-between bg-purple-500 text-white p-5 rounded-md mb-3">
               <h1>In Progress</h1>
               <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
-                {runningTasks.length}
+                {runningTasks?.length}
               </p>
             </div>
             <div className="space-y-3">
@@ -82,7 +82,7 @@ const Tasks = () => {
             <div className="flex sticky top-0 justify-between bg-purple-500 text-white p-5 rounded-md mb-3">
               <h1>Completed</h1>
               <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
-                {doneTasks.length}
+                {doneTasks?.length}
               </p>
             </div>
             <div className="space-y-3">
